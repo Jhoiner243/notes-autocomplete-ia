94afa54782870b05e41b815a1e51ef7a6012f157
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-08-14 10:25:01 -0500

    feat: integrate NotasModule into app.module.ts to enable note management functionalities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CompletionModule } from './contexts/completion/completion.module';
+import { NotasModule } from './contexts/notes/notas.module';
 import { PrismaModule } from './contexts/shared/prisma/prisma.module';
 import { UserModule } from './contexts/users/users.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, CompletionModule],
+  imports: [UserModule, PrismaModule, CompletionModule, NotasModule],
   controllers: [],
   providers: [],
 })
