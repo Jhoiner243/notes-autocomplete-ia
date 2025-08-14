478b1b76a4fd1f5e0c866a8c6a65cc414389dee8
Jhoiner243 <jhoinermartinez243@gmail.com>
2025-08-14 11:37:41 -0500

    feat: integrate Swagger documentation for the nursing notes API, enhancing API discoverability and usability

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,9 +14,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
 
-  //Global pipes para obtener las excepciones
+  const config = new DocumentBuilder()
+    .setTitle('Notas de enfermería')
+    .setDescription('API para gestionar las notas de enfermería')
+    .setVersion('1.0')
+    .addTag('Notas')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
 
   logger.log('App running in port', process.env.PORT ? process.env.PORT : 3000);
 }
